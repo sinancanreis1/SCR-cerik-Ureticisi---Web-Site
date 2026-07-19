792d92be9ecfe8c212017b79ced9d376c59219ed
Sinan Can Reis <sinancanreis@scrbilisim.com.tr>
2026-07-19 17:08:00 +0300

    Update footer copyright with SCR Bilisim link

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -5,7 +5,7 @@
 				<div class="footer-content d-flex flex-wrap justify-content-center justify-content-md-between align-items-center py-4">
 					<!-- Copyright -->
 					<div class="copyright">
-						&copy;{{ date('Y') }} Sinan Can REİS. Tüm Hakları Saklıdır.
+						&copy;{{ date('Y') }} Sinan Can REİS. Tüm Hakları Saklıdır. | Tasarım ve Kodlama: <a href="https://scrbilisim.com.tr/" target="_blank">SCR Bilişim</a>
 					</div>
 					<!-- Scroll To Top -->
 					<div id="scroll-to-top" class="scroll-to-top mt-3 mt-sm-0">
